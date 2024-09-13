c88c64e0c3f7a1c293b80826fbfe527536d3b349
khemissat <khemissat.ra@gmail.com>
2024-09-13 13:47:30 +0200

    put all in home page

---
+++ src/section/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ProductList from '../commponents/productList'
+import Navbar from '../commponents/navbar'
+export default function Home() {
+  return (
+    <>
+    <Navbar/>
+    <ProductList/>
+    </>
+    )
+}
