a407112d8042238125dec88687c8fa67f4baa247
khemissat <khemissat.ra@gmail.com>
2025-03-21 03:26:49 +0100

    fixing deployment isuue

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import { BrowserRouter as Router , Route,Routes } from 'react-router-dom'
 export default function App() {
 
   return (
-  <Router basename='/ecomweb'>
+  <Router >
     <Routes>
       <Route path="/" element={<Home/>}/>
       <Route path="/Details/:id" element={<ProductDetails/>}/>
